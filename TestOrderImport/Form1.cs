517fceff21acf79e94b6b0831c58249746f05577
Darren Swindells <darren.swindells@siemens.com>
2023-08-14 19:43:10 +0100

    Switch to DLQ connection

--- TestOrderImport/Form1.cs
+++ TestOrderImport/Form1.cs
@@ -9,55 +9,55 @@ namespace TestOrderImport
     {
 
         private ServiceProvider serviceProvider;
-		private string _webAPIEndPoint;
-   
+        private string _webAPIEndPoint;
+
         public Form1()
         {
             InitializeComponent();
-			EndPointDDL.SelectedIndex = 0;
+            EndPointDDL.SelectedIndex = 0;
         }
 
         private void InitHttpClientFactory()
         {
-			if (serviceProvider == null)
-			{
+            if (serviceProvider == null)
+            {
 
-				// Create credentials cache of potential end points
-				var credentialsCache = new CredentialCache();
+                // Create credentials cache of potential end points
+                var credentialsCache = new CredentialCache();
 
-				var uri = new Uri(_webAPIEndPoint);
-				credentialsCache.Add(uri, "NTLM", CredentialCache.DefaultNetworkCredentials);
+                var uri = new Uri(_webAPIEndPoint);
+                credentialsCache.Add(uri, "NTLM", CredentialCache.DefaultNetworkCredentials);
 
-				var service = new ServiceCollection().AddHttpClient(_webAPIEndPoint, c =>
-				{
-					c.DefaultRequestHeaders.Add("Accept", "application/json");
-					c.DefaultRequestHeaders.Add("User-Agent", "HttpClientFactory-MessageDispatcherNsc");
+                var service = new ServiceCollection().AddHttpClient(_webAPIEndPoint, c =>
+                {
+                    c.DefaultRequestHeaders.Add("Accept", "application/json");
+                    c.DefaultRequestHeaders.Add("User-Agent", "HttpClientFactory-MessageDispatcherNsc");
                     c.DefaultRequestHeaders.Add("Connection", "keep-alive");
 
 
 
                 }).ConfigurePrimaryHttpMessageHandler(() =>
-				{
-					return new HttpClientHandler()
-					{
-						//UseDefaultCredentials = true,
-						Credentials = credentialsCache,
-						PreAuthenticate = true,
-						AllowAutoRedirect = true,
-						ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; }
+                {
+                    return new HttpClientHandler()
+                    {
+                        //UseDefaultCredentials = true,
+                        Credentials = credentialsCache,
+                        PreAuthenticate = true,
+                        AllowAutoRedirect = true,
+                        ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; }
 
-					};
-				});
+                    };
+                });
 
-				serviceProvider = service.Services.BuildServiceProvider();
-			}
+                serviceProvider = service.Services.BuildServiceProvider();
+            }
 
 
-			
 
-		}
 
-		private void GetBtn_Click(object sender, EventArgs e)
+        }
+
+        private void GetBtn_Click(object sender, EventArgs e)
         {
             InitHttpClientFactory();
             GetOrderOperations();
@@ -65,26 +65,26 @@ namespace TestOrderImport
 
         private void GetOrderOperations()
         {
-			var _httpClientFactory = serviceProvider.GetService<IHttpClientFactory>();
+            var _httpClientFactory = serviceProvider.GetService<IHttpClientFactory>();
 
-			App app = new App(_httpClientFactory, _webAPIEndPoint);
+            App app = new App(_httpClientFactory, _webAPIEndPoint);
 
-			DataTable[] tbls = app.GetOrders(OrderNumberTextBox.Text.Trim());
+            DataTable[] tbls = app.GetOrders(OrderNumberTextBox.Text.Trim());
 
-			if (tbls != null && tbls.Length>0 )
-			{
-
-				this.OrdersGridView.DataSource = tbls[1].DefaultView;
-				this.OrdersGridView.Refresh();
-
-				this.OperationsGridView.DataSource = tbls[0].DefaultView;
-				this.OperationsGridView.Refresh();
-			}
-			else
+            if (tbls != null && tbls.Length > 0)
             {
-				MessageBox.Show("Order does mot exist or contains no data.", "Warning");
+
+                this.OrdersGridView.DataSource = tbls[1].DefaultView;
+                this.OrdersGridView.Refresh();
+
+                this.OperationsGridView.DataSource = tbls[0].DefaultView;
+                this.OperationsGridView.Refresh();
             }
-		}
+            else
+            {
+                MessageBox.Show("Order does mot exist or contains no data.", "Warning");
+            }
+        }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
@@ -96,9 +96,9 @@ namespace TestOrderImport
 
         }
 
-		private void EndPointDDL_SelectedIndexChanged(object sender, EventArgs e)
-		{
-			_webAPIEndPoint = EndPointDDL.SelectedItem.ToString();
-		}
-	}
+        private void EndPointDDL_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            _webAPIEndPoint = EndPointDDL.SelectedItem.ToString();
+        }
+    }
 }
